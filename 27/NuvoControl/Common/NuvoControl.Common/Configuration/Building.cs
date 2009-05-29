4470f832dd4e66fa6f91252b97bdfde709839b38
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-29 14:57:57 +0000

    CCNET build 27
    git-svn-id: https://xp-dev.com/svn/ImfeldC-NuvoControl/source/tags/Laptop@182 cc0574c9-ca68-0410-b7a3-859615599fd7

---
+++ 27/NuvoControl/Common/NuvoControl.Common/Configuration/Building.cs
@@ -0,0 +1,60 @@
+﻿/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+ ***************************************************************************************************
+ *
+ *   Project:        NuvoControl
+ *   SubProject:     NuvoControl.Common
+ *   Author:         Bernhard Limacher
+ *   Creation Date:  12.05.2009
+ *   File Name:      Building.cs
+ * 
+ ***************************************************************************************************
+ * 
+ * Revisions:
+ * 1) 12.05.2009, Bernhard Limacher: First implementation.
+ * 
+ **************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuvoControl.Common.Configuration
+{
+    [Serializable]
+    public class Building
+    {
+        #region Private Members
+
+        private List<Floor> _floors = new List<Floor>();
+
+        #endregion
+
+        #region Constructors
+
+        public Building(List<Floor> floors)
+        {
+            this._floors = floors;
+        }
+
+        #endregion
+
+        #region Private Members
+
+        public List<Floor> Floors
+        {
+            get { return _floors; }
+        }
+
+        #endregion
+    }
+}
+
+/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+**************************************************************************************************/
---
+++ 27/NuvoControl/Common/NuvoControl.Common/Configuration/Hardware.cs
@@ -0,0 +1,60 @@
+﻿/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+ ***************************************************************************************************
+ *
+ *   Project:        NuvoControl
+ *   SubProject:     NuvoControl.Common
+ *   Author:         Bernhard Limacher
+ *   Creation Date:  12.05.2009
+ *   File Name:      HardwareConfiguration.cs
+ * 
+ ***************************************************************************************************
+ * 
+ * Revisions:
+ * 1) 12.05.2009, Bernhard Limacher: First implementation.
+ * 
+ **************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuvoControl.Common.Configuration
+{
+    [Serializable]
+    public class Hardware
+    {
+        #region Private Members
+
+        private List<NuvoEssentia> _devices = new List<NuvoEssentia>();
+
+        #endregion
+
+        #region Constructors
+
+        public Hardware(List<NuvoEssentia> devices)
+        {
+            this._devices = devices;
+        }
+
+        #endregion
+
+        #region Public Interface
+
+        public List<NuvoEssentia> Devices
+        {
+            get { return _devices; }
+        }
+
+        #endregion
+    }
+}
+
+/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+**************************************************************************************************/
