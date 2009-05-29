e0450800cda9e89b514f435d536d7a307cf64b00
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-29 14:08:47 +0000

    CCNET build 23
    git-svn-id: https://xp-dev.com/svn/ImfeldC-NuvoControl/source/tags/Laptop@175 cc0574c9-ca68-0410-b7a3-859615599fd7

---
+++ 23/NuvoControl/Common/NuvoControl.Common/Configuration/SleepFunction.cs
@@ -0,0 +1,75 @@
+﻿/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+ ***************************************************************************************************
+ *
+ *   Project:        NuvoControl
+ *   SubProject:     NuvoControl.Common
+ *   Author:         Bernhard Limacher
+ *   Creation Date:  14.05.2009
+ *   File Name:      SleepFunction.cs
+ * 
+ ***************************************************************************************************
+ * 
+ * Revisions:
+ * 1) 14.05.2009, Bernhard Limacher: First implementation.
+ * 
+ **************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuvoControl.Common.Configuration
+{
+    [Serializable]
+    public class SleepFunction: Function
+    {
+        #region Private Members
+
+        private TimeSpan _sleepDuration = new TimeSpan(1, 0, 0);
+        private TimeSpan _validFrom = new TimeSpan(11, 0, 0);
+        private TimeSpan _validTo = new TimeSpan(04, 0, 0);
+
+        #endregion
+
+        #region Constructors
+
+        public SleepFunction(Guid id, Address zoneId, TimeSpan sleepDuration, TimeSpan validFrom, TimeSpan validTo)
+            : base(id, zoneId)
+        {
+            this._sleepDuration = sleepDuration;
+            this._validFrom = validFrom;
+            this._validTo = validTo;
+        }
+
+        #endregion
+
+        #region Public Interface
+
+        public TimeSpan SleepDuration
+        {
+            get { return _sleepDuration; }
+        }
+
+        public TimeSpan ValidFrom
+        {
+            get { return _validFrom; }
+        }
+
+        public TimeSpan ValidTo
+        {
+            get { return _validTo; }
+        }
+
+        #endregion
+    }
+}
+
+/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+**************************************************************************************************/
