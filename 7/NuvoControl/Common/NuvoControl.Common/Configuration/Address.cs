aa795237c7b37383fceac880595eb149c780c7c2
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-29 07:01:07 +0000

    CCNET build 7
    git-svn-id: https://xp-dev.com/svn/ImfeldC-NuvoControl/source/tags/Laptop@150 cc0574c9-ca68-0410-b7a3-859615599fd7

---
+++ 7/NuvoControl/Common/NuvoControl.Common/Configuration/Address.cs
@@ -0,0 +1,112 @@
+﻿/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+ ***************************************************************************************************
+ *
+ *   Project:        NuvoControl
+ *   SubProject:     NuvoControl.Common
+ *   Author:         Bernhard Limacher
+ *   Creation Date:  12.05.2009
+ *   File Name:      Address.cs
+ * 
+ ***************************************************************************************************
+ * 
+ * Revisions:
+ * 1) 12.05.2009, Bernhard Limacher: First implementation.
+ * 
+ **************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace NuvoControl.Common.Configuration
+{
+    [DataContract]
+    public class Address
+    {
+        #region Private Members
+
+        private int _objectId = SystemConfiguration.ID_UNDEFINED;
+        private int _deviceId = SystemConfiguration.ID_UNDEFINED;
+
+        #endregion
+
+        #region Constructors
+
+        public Address()
+        {
+        }
+
+        public Address(int deviceId, int objectId)
+        {
+            this._deviceId = deviceId;
+            this._objectId = objectId;
+        }
+
+        #endregion
+
+        #region Public Interface
+
+        [DataMember]
+        public int ObjectId
+        {
+            get { return _objectId; }
+            set { _objectId = value; }
+        }
+
+        [DataMember]
+        public int DeviceId
+        {
+            get { return _deviceId; }
+            set { _deviceId = value; }
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            Address id = obj as Address;
+            if ((object)id == null)
+                return false;
+
+            return (_deviceId == id._deviceId) && (_objectId == id._objectId);
+        }
+
+
+        public bool Equals(Address id)
+        {
+            if ((object)id == null)
+                return false;
+
+            return (_deviceId == id._deviceId) && (_objectId == id._objectId);
+        }
+
+
+        public override int GetHashCode()
+        {
+            return _deviceId ^ _objectId;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}.{1}", _deviceId, _objectId);
+        }
+
+
+        #endregion
+    }
+}
+
+/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+**************************************************************************************************/
