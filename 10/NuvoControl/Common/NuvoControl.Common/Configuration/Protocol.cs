d76cbe514df1d11176f44fbab15d9eced770b96c
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-29 07:09:34 +0000

    CCNET build 10
    git-svn-id: https://xp-dev.com/svn/ImfeldC-NuvoControl/source/tags/Laptop@154 cc0574c9-ca68-0410-b7a3-859615599fd7

---
+++ 10/NuvoControl/Common/NuvoControl.Common/Configuration/Protocol.cs
@@ -0,0 +1,75 @@
+﻿/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+ ***************************************************************************************************
+ *
+ *   Project:        NuvoControl
+ *   SubProject:     NuvoControl.Common
+ *   Author:         Bernhard Limacher
+ *   Creation Date:  19.05.2009
+ *   File Name:      ProtocolDriver.cs
+ * 
+ ***************************************************************************************************
+ * 
+ * Revisions:
+ * 1) 19.05.2009, Bernhard Limacher: First implementation.
+ * 
+ **************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuvoControl.Common.Configuration
+{
+    [Serializable]
+    public class Protocol
+    {
+        #region Private Members
+
+        private string _name = String.Empty;
+        private string _assemblyName = String.Empty;
+        private string _className = String.Empty;
+
+        #endregion
+
+        #region Constructors
+
+        public Protocol(string name, string assemblyName, string className)
+        {
+            this._name = name;
+            this._assemblyName = assemblyName;
+            this._className = className;
+        }
+
+        #endregion
+
+        #region Public Interface
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string AssemblyName
+        {
+            get { return _assemblyName; }
+        }
+
+        public string ClassName
+        {
+            get { return _className; }
+        }
+
+
+        #endregion
+    }
+}
+
+/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+**************************************************************************************************/
