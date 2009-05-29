be1ff84cbb2df27b92109f026a6940f46f30d887
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-29 12:56:13 +0000

    CCNET build 19
    git-svn-id: https://xp-dev.com/svn/ImfeldC-NuvoControl/source/tags/Laptop@167 cc0574c9-ca68-0410-b7a3-859615599fd7

---
+++ 19/NuvoControl/Common/NuvoControl.Common/Configuration/Function.cs
@@ -0,0 +1,67 @@
+﻿/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+ ***************************************************************************************************
+ *
+ *   Project:        NuvoControl
+ *   SubProject:     NuvoControl.Common
+ *   Author:         Bernhard Limacher
+ *   Creation Date:  12.05.2009
+ *   File Name:      FunctionConfiguration.cs
+ * 
+ ***************************************************************************************************
+ * 
+ * Revisions:
+ * 1) 12.05.2009, Bernhard Limacher: First implementation.
+ * 
+ **************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuvoControl.Common.Configuration
+{
+    [Serializable]
+    public abstract class Function
+    {
+        #region Private Members
+
+        private Guid _id = Guid.NewGuid();
+        private Address _zoneId = new Address(SystemConfiguration.ID_UNDEFINED, SystemConfiguration.ID_UNDEFINED);
+
+        #endregion
+
+        #region Constructors
+
+        public Function(Guid id, Address zoneId)
+        {
+            this._id = id;
+            this._zoneId = zoneId;
+        }
+
+        #endregion
+
+        #region Public Interface
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Address ZoneId
+        {
+            get { return _zoneId; }
+        }
+
+        #endregion
+    }
+}
+
+/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+**************************************************************************************************/
