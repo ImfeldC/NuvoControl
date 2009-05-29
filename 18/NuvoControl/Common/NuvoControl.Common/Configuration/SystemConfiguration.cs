41fd3811cb4eae5a617b63a8a7f2b0e675ad11bb
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-29 11:26:33 +0000

    CCNET build 18
    git-svn-id: https://xp-dev.com/svn/ImfeldC-NuvoControl/source/tags/Laptop@164 cc0574c9-ca68-0410-b7a3-859615599fd7

---
+++ 18/NuvoControl/Common/NuvoControl.Common/Configuration/SystemConfiguration.cs
@@ -0,0 +1,83 @@
+﻿/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+ ***************************************************************************************************
+ *
+ *   Project:        NuvoControl
+ *   SubProject:     NuvoControl.Common
+ *   Author:         Bernhard Limacher
+ *   Creation Date:  12.05.2009
+ *   File Name:      SystemKonfiguration.cs
+ * 
+ ***************************************************************************************************
+ * 
+ * Revisions:
+ * 1) 12.05.2009, Bernhard Limacher: First implementation.
+ * 
+ **************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuvoControl.Common.Configuration
+{
+    [Serializable]
+    public class SystemConfiguration
+    {
+        #region Constants
+
+        public const string VERSION = "1.0";
+        public const int ID_UNDEFINED = -1;
+        public const char ID_SEPARATOR = '.';
+
+
+        #endregion
+
+        #region Private Members
+
+        private Hardware _hardware = null;
+        private Graphic _graphic = null;
+        private List<Function> _functions = new List<Function>();
+
+        #endregion
+
+        #region SystemConfiguration
+
+        public SystemConfiguration(Hardware hardware, Graphic graphic, List<Function> functions)
+        {
+            this._hardware = hardware;
+            this._graphic = graphic;
+            this._functions = functions;
+        }
+
+        #endregion
+
+        #region Public Interface
+
+        public Hardware Hardware
+        {
+            get { return _hardware; }
+        }
+
+        public Graphic Graphic
+        {
+            get { return _graphic; }
+        }
+
+        public List<Function> Functions
+        {
+            get { return _functions; }
+        }
+
+        #endregion
+    }
+}
+
+/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+**************************************************************************************************/
