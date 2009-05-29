ccd1c6612b4fcc64b19781410c12bb957c3773a7
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-29 08:49:34 +0000

    CCNET build 16
    git-svn-id: https://xp-dev.com/svn/ImfeldC-NuvoControl/source/tags/Laptop@162 cc0574c9-ca68-0410-b7a3-859615599fd7

---
+++ 16/NuvoControl/Server/NuvoControl.Server.ProtocolDriver.UnitTest/Mock/SerialPortMock.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuvoControl.Server.ProtocolDriver.Interface;
+using Common.Logging;
+
+namespace NuvoControl.Server.ProtocolDriver.Test.Mock
+{
+    class SerialPortMock : ISerialPort
+    {
+        private ILog _log = LogManager.GetCurrentClassLogger();
+        private string _writeText = "";
+
+        #region ISerialPort Members
+
+        public event SerialPortEventHandler onDataReceived;
+
+        public void Open(SerialPortConnectInformation serialPortConnectInformation)
+        {
+            _log.Debug(m => m("Open called."));
+        }
+
+        public void Close()
+        {
+            _log.Debug(m => m("Close called."));
+        }
+
+        public bool IsOpen
+        {
+            get 
+            {
+                _log.Debug(m => m("IsOpen called.")); 
+                return true; 
+            }
+            
+        }
+
+        public void Write(string text)
+        {
+            _log.Debug(m => m("Write called."));
+            WriteText = text;
+        }
+
+        #endregion
+
+        //
+        // Mock Specific Methods
+        //
+
+        public void passDataToTestClass( string msg )
+        {
+            //raise the event, and pass data to next layer
+            if (onDataReceived != null)
+            {
+                onDataReceived(this,
+                  new SerialPortEventArgs(msg));
+            }
+        }
+
+        public string WriteText
+        {
+            get { return _writeText; }
+            set { _writeText = value; }
+        }
+
+    
+    }
+}
