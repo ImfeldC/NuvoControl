e82d74af920a0f9595d45b02fdd3730ae6e297bd
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-29 13:17:24 +0000

    CCNET build 21
    git-svn-id: https://xp-dev.com/svn/ImfeldC-NuvoControl/source/tags/Laptop@171 cc0574c9-ca68-0410-b7a3-859615599fd7

---
+++ 21/NuvoControl/Server/NuvoControl.Server.ProtocolDriver.UnitTest/SerialPortTest.cs
@@ -0,0 +1,118 @@
+﻿using NuvoControl.Server.ProtocolDriver;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuvoControl.Server.ProtocolDriver.Interface;
+
+namespace NuvoControl.Server.ProtocolDriver.UnitTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for SerialPortTest and is intended
+    ///to contain all SerialPortTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class SerialPortTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Open
+        ///</summary>
+        [TestMethod()]
+        public void OpenTest()
+        {
+            SerialPort target = new SerialPort();
+            SerialPortConnectInformation serialPortConnectInformation = null; 
+            target.Open(serialPortConnectInformation);
+            bool actual = target.IsOpen;
+            Assert.AreEqual(false, actual);
+        }
+
+        /// <summary>
+        ///A test for IsOpen
+        ///</summary>
+        [TestMethod()]
+        public void IsOpenTest()
+        {
+            SerialPort target = new SerialPort();
+            bool actual = target.IsOpen;
+            Assert.AreEqual(false,actual);
+        }
+
+        /// <summary>
+        ///A test for Write
+        ///</summary>
+        [TestMethod()]
+        public void WriteTest()
+        {
+            SerialPort target = new SerialPort();
+            string text = string.Empty;
+            target.Write(text);
+            bool actual = target.IsOpen;
+            Assert.AreEqual(true, actual);
+        }
+
+        /// <summary>
+        ///A test for OpenPort.
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("NuvoControl.Server.ProtocolDriver.dll")]
+        public void OpenPortTest()
+        {
+            SerialPort_Accessor target = new SerialPort_Accessor();
+            bool actual;
+            actual = target.OpenPort();
+            Assert.AreEqual(false, actual);
+        }
+
+    }
+}
