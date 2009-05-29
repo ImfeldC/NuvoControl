45dd6f3133276bb6d56eb0eee81a16665cf37aef
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-29 15:08:32 +0000

    CCNET build 28
    git-svn-id: https://xp-dev.com/svn/ImfeldC-NuvoControl/source/tags/Laptop@184 cc0574c9-ca68-0410-b7a3-859615599fd7

---
+++ 28/NuvoControl/Server/NuvoControl.Server.ProtocolDriver.UnitTest/Mock/NuvoTelegramMock.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuvoControl.Server.ProtocolDriver.Interface;
+using Common.Logging;
+
+namespace NuvoControl.Server.ProtocolDriver.Test.Mock
+{
+    class NuvoTelegramMock : INuvoTelegram
+    {
+        private ILog _log = LogManager.GetCurrentClassLogger();
+
+
+        #region INuvoTelegram Members
+
+        public event NuvoTelegramEventHandler onTelegramReceived;
+
+        public void Close()
+        {
+            _log.Debug(m => m("Close called."));
+        }
+
+        public void Open(SerialPortConnectInformation serialPortConnectInformation)
+        {
+            _log.Debug(m => m("Open called. {0}", serialPortConnectInformation.ToString()));
+        }
+
+        public void SendTelegram(string telegram)
+        {
+            _log.Debug(m => m("Send called. {0}", telegram));
+        }
+
+
+        #endregion
+
+        //
+        // Mock Specific Methods
+        //
+
+        public void passDataToTestClass(string msg)
+        {
+            //raise the event, and pass data to next layer
+            if (onTelegramReceived != null)
+            {
+                onTelegramReceived(this,
+                  new NuvoTelegramEventArgs(msg));
+            }
+        }
+
+
+    }
+}
