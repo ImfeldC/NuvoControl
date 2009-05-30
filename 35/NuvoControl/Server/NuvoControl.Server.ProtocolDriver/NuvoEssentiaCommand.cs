902a6536b9cf1eb41bb694c92116aa51490a73a6
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-30 16:28:35 +0000

    CCNET build 35
    git-svn-id: https://xp-dev.com/svn/ImfeldC-NuvoControl/source/tags/Laptop@206 cc0574c9-ca68-0410-b7a3-859615599fd7

---
+++ 35/NuvoControl/Server/NuvoControl.Server.ProtocolDriver/NuvoEssentiaCommand.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuvoControl.Server.ProtocolDriver.Interface;
+
+namespace NuvoControl.Server.ProtocolDriver
+{
+    public class NuvoEssentiaCommand : INuvoEssentiaCommand
+    {
+        private Queue<INuvoEssentiaSingleCommand> _commandList = new Queue<INuvoEssentiaSingleCommand>();
+
+        Guid _guid;
+        DateTime _createDateTime;
+
+        #region Nuvo Essentia Values
+        ENuvoEssentiaZones _zoneId = ENuvoEssentiaZones.NoZone;
+        ENuvoEssentiaSources _sourceId = ENuvoEssentiaSources.NoSource;
+        int _volume = -999;
+        #endregion
+
+
+        public NuvoEssentiaCommand(ENuvoEssentiaZones zone)
+        {
+            initMembers();
+            _zoneId = zone;
+        }
+
+        public NuvoEssentiaCommand(ENuvoEssentiaZones zone, ENuvoEssentiaSources source)
+        {
+            initMembers();
+            _zoneId = zone;
+            _sourceId = source;
+        }
+
+        public NuvoEssentiaCommand(ENuvoEssentiaZones zone, ENuvoEssentiaSources source, int volume)
+        {
+            initMembers();
+            _zoneId = zone;
+            _sourceId = source;
+            _volume = volume;
+        }
+
+
+        /// <summary>
+        /// Private method to initialize the members.
+        /// Call this method in each constructur at start.
+        /// </summary>
+        private void initMembers()
+        {
+            _commandList.Clear();
+            _createDateTime = DateTime.Now;
+            _guid = Guid.NewGuid();
+        }
+
+
+        #region INuvoEssentiaCommand Members
+
+        public Queue<INuvoEssentiaSingleCommand> commandList
+        {
+            get { return _commandList; }
+        }
+
+        public void addCommand(ENuvoEssentiaCommands command)
+        {
+            switch( command )
+            {
+                // combined commands -> not handled by the single command class
+                case ENuvoEssentiaCommands.SetInitialZoneStatus:
+                case ENuvoEssentiaCommands.VolumeDOWN2db:
+                case ENuvoEssentiaCommands.VolumeUP2db:
+
+                case ENuvoEssentiaCommands.NoCommand:
+                    break;
+
+                // default, assume single command and add them to the list
+                default:
+                    _commandList.Enqueue(new NuvoEssentiaSingleCommand(command));
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ 35/NuvoControl/Server/NuvoControl.Server.ProtocolDriver/NuvoEssentiaProtocol.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuvoControl.Server.ProtocolDriver.Interface;
+using Common.Logging;
+using System.Collections;
+using AMS.Profile;
+
+namespace NuvoControl.Server.ProtocolDriver
+{
+    class NuvoEssentiaProtocol : INuvoEssentiaProtocol
+    {
+        private ILog _log = LogManager.GetCurrentClassLogger();
+        private Profile _profile;
+
+        private INuvoTelegram _serialPort;
+
+        private Queue<INuvoEssentiaSingleCommand> _runningCommands = new Queue<INuvoEssentiaSingleCommand>();
+        private NuvoEssentiaSingleCommand _errorNuvoEssentiaCommand = new NuvoEssentiaSingleCommand(ENuvoEssentiaCommands.ErrorInCommand);
+
+        public NuvoEssentiaProtocol(INuvoTelegram nuvoTelegram)
+        {
+            _profile = new Xml("NuvoEssentiaProfile.xml");
+            _log.Debug(m => m("Open profile file: {0}", _profile.Name));
+
+            if (nuvoTelegram == null)
+                _serialPort = new NuvoTelegram(null);
+            else
+                _serialPort = nuvoTelegram;
+            _serialPort.onTelegramReceived += new NuvoTelegramEventHandler(_serialPort_onTelegramReceived);
+        }
+
+        void _serialPort_onTelegramReceived(object sender, NuvoTelegramEventArgs e)
+        {
+            INuvoEssentiaSingleCommand command = new NuvoEssentiaSingleCommand(e.Message);
+            command.IncomingCommand = e.Message;
+            command = compareIncomingCommandWithRunningCommand(command);
+
+            if (command.Valid)
+            {
+                //raise the event, and pass data to next layer
+                if (onCommandReceived != null)
+                {
+                    onCommandReceived(this,
+                      new NuvoEssentiaProtocolEventArgs(command));
+                }
+            }
+        }
+
+        private INuvoEssentiaSingleCommand compareIncomingCommandWithRunningCommand(INuvoEssentiaSingleCommand incomingCommand)
+        {
+            INuvoEssentiaSingleCommand command = null;
+
+            if (_runningCommands.Count > 0)
+            {
+                command = _runningCommands.Peek();
+                //if (command != null && compareCommands(command, incomingCommand))
+                if (command != null && compareCommandString(command.IncomingCommandTemplate, incomingCommand.IncomingCommand)
+                                    && compareZoneIds(command.ZoneId, incomingCommand.ZoneId) )
+                {
+                    // incoming command matches a previous outgoing command
+                    command = _runningCommands.Dequeue();
+                    command.IncomingCommand = incomingCommand.IncomingCommand;
+                }
+                else if (compareCommandString(_errorNuvoEssentiaCommand.IncomingCommandTemplate, incomingCommand.IncomingCommand))
+                {
+                    // incoming command indicates an error, assign them to the first command in queue
+                    command = _runningCommands.Dequeue();
+                    command.IncomingCommand = incomingCommand.IncomingCommand;
+                    _log.Error(m => m("An error returned by Nuvo Essentia to the command '{0}'", command));
+                }
+                else
+                {
+                    // no outgoing command found. This command has been issued unsolicied
+                    command = incomingCommand;
+                }
+            }
+            else
+            {
+                // no outgoing command available. This command has been issued unsolicied
+                command = incomingCommand;
+            }
+
+            return command;
+        }
+
+        #region INuvoEssentiaProtocol Members
+
+        public event NuvoEssentiaProtocolEventHandler onCommandReceived;
+
+        public void Open(SerialPortConnectInformation serialPortConnectInformation)
+        {
+            _serialPort.Open(serialPortConnectInformation);
+        }
+
+        public void Close()
+        {
+            _serialPort.Close();
+        }
+
+        public void SendCommand(string commandString)
+        {
+            NuvoEssentiaSingleCommand command = convertString2NuvoEssentiaCommand(commandString);
+            Send(command);
+        }
+
+        public void SendCommand(INuvoEssentiaSingleCommand command)
+        {
+            Send(command);
+        }
+
+        #endregion
+
+
+        private void Send(INuvoEssentiaSingleCommand command)
+        {
+            command.SendDateTime = DateTime.Now;
+            _serialPort.SendTelegram(command.OutgoingCommand);
+            _runningCommands.Enqueue(command);
+        }
+
+
+        /// <summary>
+        /// Converts the command string to a Nuvo Essentia command.
+        /// </summary>
+        /// <param name="command">Nuvo Essentia command string.</param>
+        /// <returns></returns>
+        private NuvoEssentiaSingleCommand convertString2NuvoEssentiaCommand(string command)
+        {
+            return new NuvoEssentiaSingleCommand(searchNuvoEssentiaCommand(command));
+        }
+
+        /// <summary>
+        /// Searches in the profile the command passed as string.
+        /// This can either be an incomming- or outgoing-command.
+        /// </summary>
+        /// <param name="command">Command (passed as string)</param>
+        /// <returns>Enumeration of the found command. Returns NoCommand if command string isn't available.</returns>
+        private ENuvoEssentiaCommands searchNuvoEssentiaCommand(string command)
+        {
+            string[] sectionNames = _profile.GetSectionNames();
+            foreach (string section in sectionNames)
+            {
+                string[] sectionEntries = _profile.GetEntryNames(section);
+                foreach (string entry in sectionEntries)
+                {
+                    if ( compareCommandString((string)_profile.GetValue(section, entry),command) )
+                    {
+                        _log.Debug(m => m("Entry found: Entry={0}, Section={1}", entry, section));
+                        return (ENuvoEssentiaCommands)Enum.Parse(typeof(ENuvoEssentiaCommands), section, true);
+                    }
+                }
+            }
+            // command not found
+            return ENuvoEssentiaCommands.NoCommand;
+        }
+
+        /// <summary>
+        /// Compares the received command string with a configured command string.
+        /// The comparision takes into account, that ...
+        /// - Placeholders in the configured command string are set with the specific value in the received command string
+        /// - Depending on the zone status the received command string has another length then expected
+        /// </summary>
+        /// <param name="configuredCommand">Configured command string.</param>
+        /// <param name="receivedCommand">Received command string.</param>
+        /// <returns>True of the received command string is equal to the configured command string.</returns>
+        public static bool compareCommandString(string configuredCommand, string receivedCommand)
+        {
+            // The strings are equal, when ...
+            // a) The configured string is equal to the received string OR
+            // b) The received string contains PWRON (=Power ON) and 
+            //    the length of the received string is 1 smaller than the received string
+            //    (This is because the configured string contains 3 placeholders, for either OFF or ON)
+            // c) The format of the received string is equal to the received string
+            //    (don't compare lower characters, because they are placeholders)
+
+            // Check Length first ...
+            if ((configuredCommand.Length == receivedCommand.Length) ||
+                (receivedCommand.Contains("PWRON") && (configuredCommand.Length - 1 == receivedCommand.Length))
+              )
+            {
+                // replace PWRON with PWR_ON, this allows future comparison on character level (both strings have the same size)
+                receivedCommand = receivedCommand.Replace("PWRON", "PWRXON");
+
+                // Compare each character
+                for (int i = 0; i < receivedCommand.Length; i++)
+                {
+                    if ((receivedCommand[i] != configuredCommand[i]) &&
+                        (!Char.IsLetter(configuredCommand[i]) || Char.IsUpper(configuredCommand[i])) )
+                    {
+                        // Format missmatch
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                // Size missmatch
+                return false;
+            }
+
+            // Comparison ok
+            return true;
+        }
+
+
+        public static bool compareZoneIds(ENuvoEssentiaZones zone1, ENuvoEssentiaZones zone2)
+        {
+            if ((zone1 != ENuvoEssentiaZones.NoZone) && (zone2 != ENuvoEssentiaZones.NoZone))
+            {
+                if (zone1 != zone2)
+                {
+                    // Zone ids are not the same
+                    return false;
+                }
+            }
+
+            // Either trrrhe zone ids are the same, or ...
+            // At least one zone is not defined, so we can't compare them
+            // return in this case 'true'
+            return true;
+        }
+    }
+}
