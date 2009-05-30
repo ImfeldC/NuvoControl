20f50aada9933420d6d7beb1951f137a56b354f9
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-30 09:18:28 +0000

    CCNET build 30
    git-svn-id: https://xp-dev.com/svn/ImfeldC-NuvoControl/source/tags/Laptop@190 cc0574c9-ca68-0410-b7a3-859615599fd7

---
+++ 30/NuvoControl/Common/NuvoControl.Common/Configuration/Source.cs
@@ -0,0 +1,81 @@
+﻿/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+ ***************************************************************************************************
+ *
+ *   Project:        NuvoControl
+ *   SubProject:     NuvoControl.Common
+ *   Author:         Bernhard Limacher
+ *   Creation Date:  12.05.2009
+ *   File Name:      Source.cs
+ * 
+ ***************************************************************************************************
+ * 
+ * Revisions:
+ * 1) 12.05.2009, Bernhard Limacher: First implementation.
+ * 
+ **************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuvoControl.Common.Configuration
+{
+    [Serializable]
+    public class Source
+    {
+        #region Private Members
+
+        private Address _id = new Address(SystemConfiguration.ID_UNDEFINED, SystemConfiguration.ID_UNDEFINED);
+        private string _name = String.Empty;
+        private string _picturePath = String.Empty;
+        private string _pictureType = String.Empty;
+
+        #endregion
+
+        #region Constructors
+
+        public Source(Address id, string name, string picturePath, string pictureType)
+        {
+            this._id = id;
+            this._name = name;
+            this._picturePath = picturePath;
+            this._pictureType = pictureType;
+        }
+
+        #endregion
+
+        #region Public Interface
+
+        public Address Id
+        {
+            get { return _id; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string PicturePath
+        {
+            get { return _picturePath; }
+        }
+
+        public string PictureType
+        {
+            get { return _pictureType; }
+        }
+
+        #endregion
+    }
+}
+
+/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+**************************************************************************************************/
