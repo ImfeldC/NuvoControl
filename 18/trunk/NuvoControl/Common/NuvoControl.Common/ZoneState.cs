28f572c713b2cf836a8936aed811342be8bdb163
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-30 09:02:59 +0000

    CCNET build 18
    git-svn-id: https://xp-dev.com/svn/ImfeldC-NuvoControl/source/tags/Laptop@188 cc0574c9-ca68-0410-b7a3-859615599fd7

---
+++ 18/trunk/NuvoControl/Common/NuvoControl.Common/ZoneState.cs
@@ -0,0 +1,64 @@
+﻿/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+ ***************************************************************************************************
+ *
+ *   Project:        NuvoControl
+ *   SubProject:     NuvoControl.Common
+ *   Author:         Bernhard Limacher
+ *   Creation Date:  21.05.2009
+ *   File Name:      ZoneState.cs
+ * 
+ ***************************************************************************************************
+ * 
+ * Revisions:
+ * 1) 21.05.2009, Bernhard Limacher: Initial implementation.
+ * 
+ **************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Common.Logging;
+
+using NuvoControl.Common.Configuration;
+
+
+namespace NuvoControl.Common
+{
+    public enum ZoneQuality
+    {
+        Normal = 0,
+        InError = 1,
+        OutDated = 2
+    }
+
+
+    [Serializable]
+    public class ZoneState
+    {
+        #region Fields
+
+        private static ILog _log = LogManager.GetCurrentClassLogger();
+
+        private int _volume;
+        private bool _commanded = false;
+        private Address _source;
+        private DateTime _lastUpdate;
+        private ZoneQuality _zoneQuality;
+
+        #endregion
+
+    }
+}
+
+/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+**************************************************************************************************/
+
+
---
+++ 18/trunk/NuvoControl/Server/NuvoControl.Server.ProtocolDriver.Interface/INuvoEssentiaProtocol.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuvoControl.Server.ProtocolDriver.Interface
+{
+    public delegate void NuvoEssentiaProtocolEventHandler(
+              object sender, NuvoEssentiaProtocolEventArgs e);
+
+    public class NuvoEssentiaProtocolEventArgs : EventArgs
+    {
+        INuvoEssentiaCommand _command;
+
+        public INuvoEssentiaCommand Command
+        {
+            get { return _command; }
+        }
+
+        public NuvoEssentiaProtocolEventArgs(INuvoEssentiaCommand command)
+        {
+            _command = command;
+        }
+
+    }
+
+    public interface INuvoEssentiaCommand : IComparable
+    {
+        Guid Guid { get; }
+        ENuvoEssentiaCommands Command { get; }
+        bool Valid { get; }
+
+        DateTime CreatedDateTime { get; }
+        DateTime SendDateTime { get; set; }
+        DateTime ReceiveDateTime { get; }
+
+        string OutgoingCommandTemplate { get; }
+        string OutgoingCommand { get; }
+        string IncomingCommandTemplate { get; }
+        string IncomingCommand { get; set; }
+
+        ENuvoEssentiaZones ZoneId { get; }
+        ENuvoEssentiaSources SourceId { get; }
+        EZonePowerStatus PowerStatus { get; }
+        EIRCarrierFrequency IrCarrierFrequencySource( ENuvoEssentiaSources source );
+        int VolumeLevel { get; }
+        int BassLevel { get; }
+        int TrebleLevel { get; }
+        EVolumeResetStatus VolumeResetStatus { get; }
+        EDIPSwitchOverrideStatus DIPSwitchOverrideStatus { get; }
+        ESourceGroupStatus SourceGrupStatus { get; }
+        string FirmwareVersion { get; }
+    }
+
+    public interface INuvoEssentiaProtocol
+    {
+
+        event NuvoEssentiaProtocolEventHandler onCommandReceived;
+
+        void Open(SerialPortConnectInformation serialPortConnectInformation);
+
+        void Close();
+
+        /// <summary>
+        /// Send the command passed as string to the lower system.
+        /// If the command string cannot be converted into a valid
+        /// Nuvo Essentia command, nothing is sent to the lower 
+        /// system.
+        /// </summary>
+        /// <param name="command">Nuvo Essentia command (as string). Without starting *-sign and leading '<BR>'sign.</param>
+        void SendCommand(string command);
+
+        /// <summary>
+        /// Send the command passed as object to the lower system.
+        /// </summary>
+        /// <param name="command">Nuvo Essentia command.</param>
+        void SendCommand(INuvoEssentiaCommand command);
+    
+    }
+}
