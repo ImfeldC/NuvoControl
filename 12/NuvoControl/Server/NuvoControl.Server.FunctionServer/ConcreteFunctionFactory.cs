98dcbf2acf6b499fbbcbaba20137c551ea168b1d
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-09-21 21:55:10 +0000

    CCNET build 12
    git-svn-id: https://xp-dev.com/svn/ImfeldC-NuvoControl/source/tags/NuvoControlBuildCD@973 cc0574c9-ca68-0410-b7a3-859615599fd7

---
+++ 12/NuvoControl/Server/NuvoControl.Server.FunctionServer/ConcreteFunctionFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuvoControl.Common.Configuration;
+using NuvoControl.Server.ZoneServer;
+
+namespace NuvoControl.Server.FunctionServer
+{
+    class ConcreteFunctionFactory
+    {
+        public static IConcreteFunction instantiateConcreteFuntion(Function function, IZoneServer zoneServer)
+        {
+            if (typeof(SleepFunction) == function.GetType())
+            {
+                return new ConcreteSleepFunction((SleepFunction)function, zoneServer);
+            }
+            else if(typeof(AlarmFunction) == function.GetType())
+            {
+                return new ConcreteAlarmFunction((AlarmFunction)function, zoneServer);
+            }
+            return null;
+        }
+    }
+}
---
+++ 12/NuvoControl/Server/NuvoControl.Server.FunctionServer/IConcreteFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuvoControl.Common.Configuration;
+
+namespace NuvoControl.Server.FunctionServer
+{
+    interface IConcreteFunction
+    {
+        /// <summary>
+        /// Returns the underlying configuration data for this function object.
+        /// </summary>
+        Function Function { get; }
+
+        /// <summary>
+        /// Returns true, if the function is active.
+        /// </summary>
+        bool Active { get; }
+
+        /// <summary>
+        /// This method is periodically called, to re-calculate the current function.
+        /// If required, this method sends commands to the zones, to change their state.
+        /// </summary>
+        /// <param name="aktTime">Actual date and time, as base for the calculation.</param>
+        void calculateFunction(DateTime aktTime);
+
+    }
+}
