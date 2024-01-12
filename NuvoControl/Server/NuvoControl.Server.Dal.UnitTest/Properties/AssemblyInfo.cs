fdd75a2cb8fccabc6e740905e86901ce3ed33da7
Christian Imfeld <christian.imfeld@siemens.com>
2024-01-12 14:40:41 +0100

    check-in before removal. Migration to .NEt 8.0 failed.

--- NuvoControl/Server/NuvoControl.Server.Dal.UnitTest/Properties/AssemblyInfo.cs
+++ NuvoControl/Server/NuvoControl.Server.Dal.UnitTest/Properties/AssemblyInfo.cs
@@ -19,17 +19,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NuvoControl.Server.Dal.UnitTest")]
 [assembly: AssemblyCulture("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4e714c30-b01b-4ee0-895c-b8da8fbc6a79")]
-
-[assembly: AssemblyVersionAttribute("2.0.0.0")]
-[assembly: AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: AssemblyCompanyAttribute("NuvoControl")]
-[assembly: AssemblyCopyrightAttribute("Copyright © B.Limacher and Ch.Imfeld")]
