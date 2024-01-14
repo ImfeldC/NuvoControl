eb05bb72f1f18d39767e7cf2147a19c16f046dde
Christian Imfeld <christian.imfeld@siemens.com>
2024-01-14 11:48:35 +0100

    Add Settings class to WebConsole project.

---
+++ NuvoControl/Server/NuvoControl.Server.WebConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f7d6d8fe-180e-4857-9496-99fd31e8dcb0")]
